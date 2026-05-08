ab6a2287cfefbd63360ece76d99e99a4795c4f28
dante007d <vinnv902@gmail.com>
2026-05-09 03:21:58 +0530

    Fix runtime error: clean up root component for SPA mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { RouterProvider } from '@tanstack/react-router'
+import { getRouter } from './router'
 import './styles.css'
 
-const router = createRouter({ routeTree })
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
+const router = getRouter()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
